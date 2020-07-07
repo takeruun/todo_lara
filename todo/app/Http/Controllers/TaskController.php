1f11d9e59a9575bd2d7a297060fab2d744282756
takeru.hashimoto <hstake25@gmail.com>
2020-07-07 23:23:08 +0900

    Folderのcreate ルーティン作成

--- todo/app/Http/Controllers/TaskController.php
+++ todo/app/Http/Controllers/TaskController.php
@@ -11,12 +11,12 @@ class TaskController extends Controller
     //
     public function index(int $id){
         $folders = Folder::all();
-        $current_folder = Task::find($id);
+        $current_folder = Folder::find($id);
         $tasks = $current_folder->tasks()->get();
 
         return view('tasks/index',[
             'folders' => $folders,
-            'current_folder_id' => $id,
+            'current_folder_id' => $current_folder->id,
             'tasks' => $tasks,
             ]);
     }
