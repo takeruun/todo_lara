a4c9954960050625c485c064883c3423971e1353
takeru.hashimoto <hstake25@gmail.com>
2020-07-07 23:58:54 +0900

    タスク form 追加

--- todo/app/Http/Controllers/TaskController.php
+++ todo/app/Http/Controllers/TaskController.php
@@ -5,10 +5,10 @@ namespace App\Http\Controllers;
 use App\Folder;
 Use App\Task;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateTask;
 
 class TaskController extends Controller
 {
-    //
     public function index(int $id){
         $folders = Folder::all();
         $current_folder = Folder::find($id);
@@ -19,5 +19,26 @@ class TaskController extends Controller
             'current_folder_id' => $current_folder->id,
             'tasks' => $tasks,
             ]);
+    }
+
+    public function showCreateForm(int $id)
+    {
+        return view('tasks/create', [
+            'folder_id' => $id
+        ]);
+    }
+
+    public function create(int $id, CreateTask $request){
+        $current_folder = Folder::find($id);
+
+        $task = Task::new();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date();
+
+        $current_folder->tasks()->save($task);
+
+        return redirect()->route('tasks.index',[
+            'id' => $current_folder->id,
+        ]);
     }
 }
